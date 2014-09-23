263f8d50ed53db1f0e989787ab8d6965dfeb92dc
Daniel Byrne <byrnedj12@gmail.com>
2014-09-23 15:43:28 -0400

    added my name

--- NamePrinter.java
+++ NamePrinter.java
@@ -1,8 +1,7 @@
 public class NamePrinter {
 
 	public static void main(String[] args) {
-		// for each person on the team, write the code to print your
-		// name to the console.
+		System.out.println("Daniel Byrne");
 
 	}
 
