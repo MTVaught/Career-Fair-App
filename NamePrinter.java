8db190fab98940c899f3828fd6a6f33847707eb6
Andrew Hanson <andrewh@mtu.edu>
2014-09-24 15:15:32 -0400

    andrew commit

--- NamePrinter.java
+++ NamePrinter.java
@@ -4,8 +4,8 @@ public class NamePrinter {
 		// for each person on the team, write the code to print your
 		// name to the console.
 		
-		//Print Matthew Vaught to the console
-		System.out.println("Matthew Vaught");
+		//Print Andrew Hanson to the console
+		System.out.println("Andrew Hanson");
 	}
 
 }
