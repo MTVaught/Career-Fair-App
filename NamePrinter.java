f4a6d5403a7cab8f9712ca9d6fbe4d0c86e2a64f
andrewhmtu <andrewh@mtu.edu>
2014-09-24 15:51:40 -0400

    Revert "andrew commit"

--- NamePrinter.java
+++ NamePrinter.java
@@ -4,8 +4,8 @@ public class NamePrinter {
 		// for each person on the team, write the code to print your
 		// name to the console.
 		
-		//Print Andrew Hanson to the console
-		System.out.println("Andrew Hanson");
+		//Print Matthew Vaught to the console
+		System.out.println("Matthew Vaught");
 	}
 
 }
