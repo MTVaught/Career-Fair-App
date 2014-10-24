8822da78b760d28df55f42003c7fe9e14c7f4f3d
Matthew Vaught <vaughtmt@gmail.com>
2014-10-23 20:25:25 -0400

    Removed Unused Fragments and Actions Still need to remove associated classes and xmls

--- src/com/example/careerfair/MainActivity.java
+++ src/com/example/careerfair/MainActivity.java
@@ -67,19 +67,11 @@ public class MainActivity extends Activity implements
 		FragmentTransaction ft = fragmentManager.beginTransaction();
 		
 		switch(position){
+		
 		case 0:
-			ft.replace(R.id.container, PlaceholderFragment.newInstance(position)).commit();
-			break;
-		case 1:
-			ft.replace(R.id.container, PlaceholderFragment.newInstance(position)).commit();
-			break;
-		case 2:
-			ft.replace(R.id.container, PlaceholderFragment.newInstance(position)).commit();
-			break;
-		case 3:
 			ft.replace(R.id.container, CompanyListFragment.newInstance(position)).commit();
 			break;
-		case 4:
+		case 1:
 			ft.replace(R.id.container, MultiPurposeGymFragment.newInstance(position)).commit();
 			break;
 		}
@@ -112,18 +104,9 @@ public class MainActivity extends Activity implements
 	public void onSectionAttached(int number) {
 		switch (number) {
 		case 0:
-			mTitle = getString(R.string.title_section1);
-		    break;
-		case 1:
-			mTitle = getString(R.string.title_section2);
-			break;
-		case 2:
-			mTitle = getString(R.string.title_section3);
-			break;
-		case 3:
 			mTitle = getString(R.string.ListView);
 			break;
-		case 4:
+		case 1:
 			mTitle = getString(R.string.title_multipurposegym);
 			break;
 		}
@@ -161,44 +144,6 @@ public class MainActivity extends Activity implements
 		return super.onOptionsItemSelected(item);
 	}
 
-	/**
-	 * A placeholder fragment containing a simple view.
-	 */
-	public static class PlaceholderFragment extends Fragment {
-		/**
-		 * The fragment argument representing the section number for this
-		 * fragment.
-		 */
-		private static final String ARG_SECTION_NUMBER = "section_number";
-
-		/**
-		 * Returns a new instance of this fragment for the given section number.
-		 */
-		public static PlaceholderFragment newInstance(int sectionNumber) {
-			PlaceholderFragment fragment = new PlaceholderFragment();
-			Bundle args = new Bundle();
-			args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-			fragment.setArguments(args);
-			return fragment;
-		}
-
-		public PlaceholderFragment() {
-		}
-
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container,
-				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_main, container,
-					false);
-			return rootView;
-		}
-
-		@Override
-		public void onAttach(Activity activity) {
-			super.onAttach(activity);
-			((MainActivity) activity).onSectionAttached(getArguments().getInt(
-					ARG_SECTION_NUMBER));
-		}
-	}
+	
 
 }
