73771ca846d5b2c1d36edd6d995db10140d22c9a
Hannah Wilder <hewample@mtu.edu>
2014-11-09 23:35:02 -0500

    Added comments to all assigned classes (Company, CompanyTest, DbAccess, DbAccessTest, ExternalDbOpenHelper, Major, Major Test)

--- src/com/JUnit/CompanyTest.java
+++ src/com/JUnit/CompanyTest.java
@@ -20,11 +20,8 @@ public class CompanyTest extends TestCase {
 	 */
 	@Override
 	protected void setUp() throws Exception {
-		// MainActivity app = new MainActivity();
-		// ExternalDbOpenHelper helper = new ExternalDbOpenHelper(
-		// app.getBaseContext(), "careerFairDB.db" );
-		// db = helper.openDataBase();
 
+		//Initialize some values for testing
 		ArrayList<Major> majors = new ArrayList<Major>();
 		ArrayList<String> positions = new ArrayList<String>();
 		ArrayList<String> workAuths = new ArrayList<String>();
@@ -43,11 +40,13 @@ public class CompanyTest extends TestCase {
 		super.setUp();
 	}
 
+	//Destroys objects after test
 	@Override
 	protected void tearDown() throws Exception {
 		super.tearDown();
 	}
 
+	//Test constructor
 	public void testCompany() {
 
 		Company test = new Company("aCompany", "www.aCompanyWebsite.com", "1",
@@ -57,15 +56,18 @@ public class CompanyTest extends TestCase {
 
 	}
 
+	//Tests the getMajors method
 	public void testGetMajors() {
 		ArrayList<Major> majorList = mTest.getMajors();
+		
+		//Make sure something is returned
 		Assert.assertNotNull(majorList);
 
 		// getMajors should return a copy of the ArrayList, not the same
 		// arrayList
 		Assert.assertTrue(majorList.toString() != mMajors.toString());
 
-		// Check majors
+		// Check majors returned
 		Assert.assertTrue(majorList.size() == mMajors.size());
 		Assert.assertTrue(majorList.get(0).getName() == mMajors.get(0)
 				.getName());
@@ -74,22 +76,28 @@ public class CompanyTest extends TestCase {
 
 	}
 
+	//Tests the getPositions method
 	public void testGetPositions() {
 		ArrayList<String> positions = mTest.getPositions();
+		
+		//Make sure something is returned
 		Assert.assertNotNull(positions);
 
 		// getPositions should return a copy of the ArrayList, not the same
 		// arrayList
 		Assert.assertTrue(positions.toString() != mPositions.toString());
 
-		// Check positions
+		// Check positions returned
 		Assert.assertTrue(positions.size() == mPositions.size());
 		Assert.assertTrue(positions.get(0) == mPositions.get(0));
 	}
 
+	//Tests the getWorkAuth method
 	public void testGetWorkAuth() {
 
 		ArrayList<String> workAuth = mTest.getWorkAuth();
+		
+		//Make sure something is returned
 		Assert.assertNotNull(workAuth);
 
 		// getWorkAuth should return a copy of the ArrayList, not the same
@@ -101,27 +109,47 @@ public class CompanyTest extends TestCase {
 		Assert.assertTrue(workAuth.get(0) == mWorkAuths.get(0));
 	}
 
+	//Tests the get name method
 	public void testGetName() {
 		String name = mTest.getName();
+		
+		//Make sure something is returned
 		Assert.assertNotNull(name);
+		
+		//Compare with expected value
 		Assert.assertEquals(mTest.getName(), "aCompany");
 	}
 
+	//Tests the getWebsite method
 	public void testGetWebsite() {
 		String website = mTest.getWebsite();
+		
+		//Make sure something is returned
 		Assert.assertNotNull(website);
+		
+		//Compare with expected value
 		Assert.assertEquals(mTest.getWebsite(), "www.aCompanyWebsite.com");
 	}
 
+	//Tests the getTableNum method
 	public void testGetTableNum() {
 		String tableNum = mTest.getTableNum();
+		
+		//Make sure something is returned
 		Assert.assertNotNull(tableNum);
+		
+		//Compare with expected value
 		Assert.assertEquals(mTest.getTableNum(), "1");
 	}
 
+	//Tests the getTableNum method
 	public void testGetRoom() {
 		String room = mTest.getRoom();
+		
+		//Make sure something is returned
 		Assert.assertNotNull(room);
+		
+		//Compare with expected value
 		Assert.assertEquals(mTest.getRoom(), "Wood Gym");
 	}
 
