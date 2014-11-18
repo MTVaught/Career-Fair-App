fd682491e6004eadacde9bbbb47ee4834a7e2055
zichengl <zichengl@mtu.edu>
2014-11-18 18:04:07 -0500

    1)Totally implement the back arrow button 2)Fix the layout of company_list.xml

--- src/com/fragments/MainActivity.java
+++ src/com/fragments/MainActivity.java
@@ -103,17 +103,22 @@ public class MainActivity extends Activity implements
 			ft.replace(
 					R.id.container,
 					CompanyListFragment.newInstance(position,
-							filteredCompanyNames)).commit();
+							filteredCompanyNames));
+			ft.addToBackStack(null);
+			ft.commit();
 			break;
 		//Displays Multipurpose Gym map
 		case 1:
 			ft.replace(R.id.container,
-					MultiPurposeGymFragment.newInstance(position)).commit();
+					MultiPurposeGymFragment.newInstance(position));
+			ft.addToBackStack(null);
+			ft.commit();
 			break;
 		//Displays Wood Gym map
 		case 2:
-			ft.replace(R.id.container, WoodGymFragment.newInstance(position))
-					.commit();
+			ft.replace(R.id.container, WoodGymFragment.newInstance(position));
+			ft.addToBackStack(null);
+			ft.commit();
 			break;
 		//Displays the filter
 		case 3:
@@ -124,7 +129,9 @@ public class MainActivity extends Activity implements
 			ft.replace(
 					R.id.container,
 					PreferencesViewFragment.newInstance(position, MajorAbbrevs,
-							WorkAuths, Positions)).commit();
+							WorkAuths, Positions));
+			ft.addToBackStack(null);
+			ft.commit();
 			break;
 		}
 
@@ -143,8 +150,9 @@ public class MainActivity extends Activity implements
 		Company clickedCompany = filteredCompanyList.get(position);
 		mTitle = clickedCompany.getName();
 		ft.replace(R.id.container,
-				CompanyReaderFragment.newInstance(position, clickedCompany))
-				.commit();
+				CompanyReaderFragment.newInstance(position, clickedCompany));
+		ft.addToBackStack(null);
+		ft.commit();
 		inCompanyView = true;
 	}
 
@@ -232,24 +240,6 @@ public class MainActivity extends Activity implements
 		return super.onOptionsItemSelected(item);
 	}
 
-	/**
-	 * onBackPressed
-	 * Controls the actions taken when the back arrow is pressed while in the android application.
-	 * By default, no action is taken unless the user is in the Detailed Company View, then they
-	 * are taken to the List of Companies.
-	 */
-	@Override
-	public void onBackPressed() {
-		Log.d("CDA", "onBackPressed Called");
-		if (inCompanyView) {
-			FragmentManager fragmentManager = getFragmentManager();
-			FragmentTransaction ft = fragmentManager.beginTransaction();
-			inCompanyView = false;
-			ft.replace(R.id.container, CompanyListFragment.newInstance(0))
-					.commit();
-		} else {
-		}
-	}
 
 	/**
 	 * databaseOpen
--- src/com/fragments/MultiPurposeGymFragment.java
+++ src/com/fragments/MultiPurposeGymFragment.java
@@ -128,8 +128,9 @@ public class MultiPurposeGymFragment extends Fragment {
 						}
 						MainActivity.appMainActivity.setTitle( clickedCompany.getName() );
 						ft.replace(R.id.container,
-								CompanyReaderFragment.newInstance( clickedCompany ))
-								.commit();
+								CompanyReaderFragment.newInstance( clickedCompany ));
+						ft.addToBackStack(null);
+						ft.commit();
 						MainActivity.appMainActivity.inCompanyView = true;
 					}
 				});
